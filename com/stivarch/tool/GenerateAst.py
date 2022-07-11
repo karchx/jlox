f2f31d383098ce158f5951250800b863f82fa564
ksandoval <ksandoval@aseguate.com>
2022-07-11 12:55:45 -0600

    feat: script in python

--- com/stivarch/tool/GenerateAst.py
+++ com/stivarch/tool/GenerateAst.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
-
 import sys
 
-def main(args):
-    if(len(args) < 1):
-        print("Usage: generate_ast <output directory>")
-        exit(64)
-    outputDir = args[1]
-    defineAst(outputDir, "Expr", )
+class GenerateAst:
+    def __init__(self, args):
+        self.args = args
 
+    def defineAst(self, outputDir, baseName, types):
+        path = outputDir + "/" + baseName + ".java"
+        print(path)
 
-main(sys.argv)
+    def main(self):
+        if(len(self.args) == 1):
+            print("Usage GenerateAst <output directory>")
+            exit(64)
 
+        outputDir = self.args[1]
+
+        self.defineAst(outputDir, "Expr", (
+                "Binary : Expr left, Token opeartor, Expr right",
+	            "Grouping : Expr expression",
+	            "Literal : Object value",
+	            "Unary : Token operator, Expr right"))
+
+generateAst = GenerateAst(sys.argv)
+generateAst.main()
