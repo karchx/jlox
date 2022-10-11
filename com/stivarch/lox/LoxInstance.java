6991f3b655310eae5902b9a5a3b91e730e0f30f6
Стив <kenethra.74@gmail.com>
2022-10-10 19:24:30 -0600

    feat: instances properties

--- com/stivarch/lox/LoxInstance.java
+++ com/stivarch/lox/LoxInstance.java
@@ -1,10 +1,25 @@
 package com.stivarch.lox;
 
+import java.util.Map;
+import java.util.HashMap;
+
 class LoxInstance {
   private LoxClass klass;
+  private final Map<String, Object> fields = new HashMap<>();
 
   LoxInstance(LoxClass klass) {
     this.klass = klass;
+  }
+
+  Object get(Token name) {
+    if (fields.containsKey(name.lexeme)) {
+      return fields.get(name.lexeme);
+    }
+    throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
+  }
+
+  void set(Token name, Object value) {
+    fields.put(name.lexeme, value);
   }
 
   @Override
